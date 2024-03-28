003297ce609a68430faa9e08fa81e347beddfedb
Shaheena Nazeer <Shaheenanazeer2@gmail.com>
2024-03-28 11:47:40 -0700

    created number guessing game

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumbre = Math.floor(Math.random() * 6 + 1);
+
+console.log(randomNumbre);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-6:"
+    }
+]);
+if (answers.userGuessedNumber === randomNumbre) {
+    console.log("congtratulations! you guessed right number.");
+}
+else {
+    console.log("yuo guessed wrong number");
+}
